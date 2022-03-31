15509c948d7670bc2c1cb9a0f2d88c8058b0f455
Yurii Leshchyshyn <yurii_leshchyshyn@epam.com>
2022-03-31 16:53:36 +0300

    feat: removed old CreateTask component

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,4 +1,3 @@
-import { CreateTask } from '../components/CreateTask';
 import { VolunteerTasks } from '../components/VolunteerTasks';
 import { ROLES } from '../constants/uiConfig';
 import { CreateSubtask } from '../screens/CreateSubtask';
@@ -13,7 +12,6 @@ export const routes = [
   { link: 'tasks', component: TasksList, isAuthorized: true },
   { link: 'my-tasks', component: VolunteerTasks, isAuthorized: true },
   { link: 'create-subtask/:taskId', component: CreateSubtask, isAuthorized: true },
-  { link: 'create-task', component: CreateTask, isAuthorized: true, role: ROLES.operator },
   { link: 'profile', component: Profile, isAuthorized: true },
   { link: 'sign-up', component: SignUp },
   { link: 'login', component: SignIn }
