90d2dda267e8b61d08105cd03b93aba2ec42cebf
Mykola Makhin <mvmn.inbox@gmail.com>
2022-04-30 09:46:11 +0300

    Handle login errors - removed unnecessary action.

--- src/constants/user.js
+++ src/constants/user.js
@@ -3,4 +3,3 @@ export const SIGN_IN = 'SIGN_IN';
 export const SET_LOGGED_OUT = 'SET_LOGGED_OUT';
 export const SET_USER = 'SET_USER';
 export const SET_NEW_PASSWORD = 'SET_NEW_PASSWORD';
-export const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
--- src/sagas/authSaga.js
+++ src/sagas/authSaga.js
@@ -2,7 +2,6 @@ import { push } from 'connected-react-router';
 import { call, put, takeLatest } from 'redux-saga/effects';
 
 import { setLoggedIn } from '../actions/user';
-import { setLoginError } from '../actions/user';
 import { authenticate } from '../api';
 import { ACCESS_TOKEN } from '../constants/uiConfig';
 import { SIGN_IN } from '../constants/user';
@@ -11,12 +10,9 @@ function* authSaga(test) {
   try {
     const data = yield call(() => authenticate(test.payload));
     if (data && data.success) {
-      yield put(setLoginError(null));
       yield sessionStorage.setItem(ACCESS_TOKEN, data.accessToken);
       yield put(setLoggedIn(true));
       yield put(push('/'));
-    } else {
-      yield put(setLoginError(data ? data.error : 'unknown'));
     }
   } catch (e) {
     console.log(e);
