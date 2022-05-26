4693f284fd5107064c7ebdf02ce807237b6d0f17
Mykola Makhin <mvmn.inbox@gmail.com>
2022-05-27 02:35:29 +0300

    Skeleton of category and product management page.

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,5 +1,6 @@
 import { VolunteerTasks } from '../components/VolunteerTasks';
 import { ROLES } from '../constants/uiConfig';
+import { CategoryProductMgmt } from '../screens/CategoryProductMgmt/CategoryProductMgmt';
 import { ChangePassword } from '../screens/ChangePassword';
 import { CreateSubTask } from '../screens/CreateSubTask';
 import { EditSubTask } from '../screens/EditSubTask';
@@ -15,6 +16,7 @@ export const routes = [
   { link: 'users', component: UserList, isAuthorized: true, role: ROLES.operator },
   { link: 'tasks', component: TasksList, isAuthorized: true },
   { link: 'stores', component: StoresList, isAuthorized: true },
+  { link: 'categories-products', component: CategoryProductMgmt, isAuthorized: true },
   { link: 'my-tasks', component: VolunteerTasks, isAuthorized: true },
   { link: 'subtasks/:taskId', component: EditSubTask, isAuthorized: true },
   { link: 'create-subtask/:taskId', component: CreateSubTask, isAuthorized: true },
