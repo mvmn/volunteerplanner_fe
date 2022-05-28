6d50ce4e71ac06f23b99b4c6b9e02b5cf74febb4
Serhii Kovalchuk <serhii.the.dev@gmail.com>
2022-05-28 15:02:44 +0300

    feat: add API integration for signup form

--- src/screens/SignUp/SignUp.jsx
+++ src/screens/SignUp/SignUp.jsx
@@ -1,40 +1,74 @@
 import 'yup-phone';
 
-import { Button, Container, TextField } from '@mui/material';
+import {
+  Button,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField
+} from '@mui/material';
 import { useFormik } from 'formik';
 import { useDispatch } from 'react-redux';
 import * as yup from 'yup';
 
-import { setLoggedIn } from '../../actions/user';
+import { signIn } from '../../actions/user';
+import { signUp } from '../../api/auth';
 import dictionary from '../../dictionary';
 import { yupPatterns } from '../../helpers/validation';
 import styles from './SignUp.module.scss';
 
+const roles = [
+  {
+    value: 'root',
+    name: 'Адміністратор'
+  },
+  {
+    value: 'operator',
+    name: 'Оператор'
+  },
+  {
+    value: 'requestor',
+    name: 'Запитувач'
+  },
+  {
+    value: 'volunteer',
+    name: 'Волонтер'
+  }
+];
+
+const initialValues = {
+  phoneNumber: '',
+  password: '',
+  confirmPassWord: '',
+  userName: '',
+  displayName: '',
+  role: roles[roles.length - 1].name
+};
+
+const validationSchema = yup.object().shape({
+  phoneNumber: yupPatterns('phoneNumber'),
+  password: yupPatterns('signUpPass'),
+  confirmPassWord: yupPatterns('confirmPassWord'),
+  displayName: yupPatterns('displayName')
+});
+
 export const SignUp = () => {
   const dispatch = useDispatch();
 
-  const initialValues = {
-    phoneNumber: '',
-    password: '',
-    confirmPassWord: '',
-    email: '',
-    userName: '',
-    displayName: ''
+  const setLoginError = error => {
+    if (error) {
+      console.error('Could not authenticate after sign up:', error);
+    }
   };
-
-  const validationSchema = yup.object().shape({
-    phoneNumber: yupPatterns('phoneNumber'),
-    password: yupPatterns('signUpPass'),
-    confirmPassWord: yupPatterns('confirmPassWord'),
-    email: yupPatterns('email'),
-    displayName: yupPatterns('displayName')
-  });
 
   const formik = useFormik({
     initialValues,
     validationSchema,
-    async onSubmit() {
-      dispatch(setLoggedIn());
+    async onSubmit(data) {
+      const { phoneNumber } = await signUp(data);
+      dispatch(signIn({ phoneNumber, password: data.password, setLoginError }));
     }
   });
 
@@ -43,88 +77,88 @@ export const SignUp = () => {
   return (
     <Container>
       <h2>{dictionary.signUp}</h2>
+
       <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.field_box}>
-          <TextField
-            id='displayName'
-            name='displayName'
-            value={values.displayName}
-            classes={{ root: styles.root }}
-            label={dictionary.displayName}
-            type='text'
+        <TextField
+          id='displayName'
+          name='displayName'
+          value={values.displayName}
+          classes={{ root: styles.root }}
+          label={dictionary.displayName}
+          type='text'
+          size='small'
+          margin='normal'
+          onChange={handleChange}
+          error={Boolean(errors.displayName)}
+          helperText={errors.displayName}
+          autoComplete='username'
+          fullWidth
+        />
+        <TextField
+          id='phoneNumber'
+          name='phoneNumber'
+          value={values.phoneNumber}
+          classes={{ root: styles.root }}
+          label={dictionary.phoneNumber}
+          type='tel'
+          size='small'
+          margin='normal'
+          onChange={handleChange}
+          error={Boolean(errors.phoneNumber)}
+          helperText={errors.phoneNumber}
+          autoComplete='phone'
+          fullWidth
+        />
+        <TextField
+          id='password'
+          name='password'
+          value={values.password}
+          classes={{ root: styles.root }}
+          label={dictionary.password}
+          type='password'
+          size='small'
+          margin='normal'
+          onChange={handleChange}
+          error={Boolean(errors.password)}
+          helperText={errors.password}
+          autoComplete='new-password'
+          fullWidth
+        />
+        <TextField
+          id='confirmPassWord'
+          name='confirmPassWord'
+          value={values.confirmPassWord}
+          classes={{ root: styles.root }}
+          label={dictionary.confirmPassWord}
+          type='password'
+          size='small'
+          margin='normal'
+          onChange={handleChange}
+          error={Boolean(errors.confirmPassWord)}
+          helperText={errors.confirmPassWord}
+          autoComplete='new-password'
+          fullWidth
+        />
+        <FormControl size='small' margin='normal' fullWidth>
+          <InputLabel id='role-label'>{dictionary.role}</InputLabel>
+          <Select
+            labelId='role-label'
+            id='role'
             size='small'
-            margin='normal'
+            name='role'
+            sx={{ textAlign: 'left' }}
+            value={values.role}
+            label={dictionary.role}
             onChange={handleChange}
-          />
-          <div className={styles.errors_box}>
-            <span className={styles.errors}>{errors.displayName}</span>
-          </div>
-        </div>
-        <div className={styles.field_box}>
-          <TextField
-            id='phoneNumber'
-            name='phoneNumber'
-            value={values.phoneNumber}
-            classes={{ root: styles.root }}
-            label={dictionary.phoneNumber}
-            type='tel'
-            size='small'
-            margin='normal'
-            onChange={handleChange}
-          />
-          <div className={styles.errors_box}>
-            <span className={styles.errors}>{errors.phoneNumber}</span>
-          </div>
-        </div>
-        <div className={styles.field_box}>
-          <TextField
-            id='email'
-            name='email'
-            value={values.email}
-            classes={{ root: styles.root }}
-            label={dictionary.email}
-            type='email'
-            size='small'
-            margin='normal'
-            onChange={handleChange}
-          />
-          <div className={styles.errors_box}>
-            <span className={styles.errors}>{errors.email}</span>
-          </div>
-        </div>
-        <div className={styles.field_box}>
-          <TextField
-            id='password'
-            name='password'
-            value={values.password}
-            classes={{ root: styles.root }}
-            label={dictionary.password}
-            type='password'
-            size='small'
-            margin='normal'
-            onChange={handleChange}
-          />
-          <div className={styles.errors_box}>
-            <span className={styles.errors}>{errors.password}</span>
-          </div>
-        </div>
-        <div className={styles.field_box}>
-          <TextField
-            id='confirmPassWord'
-            name='confirmPassWord'
-            value={values.confirmPassWord}
-            classes={{ root: styles.root }}
-            label={dictionary.confirmPassWord}
-            type='password'
-            size='small'
-            margin='normal'
-            onChange={handleChange}
-          />
-          <div className={styles.errors_box}>
-            <span className={styles.errors}>{errors.confirmPassWord}</span>
-          </div>
-        </div>
-        <Button variant='outlined' type='submit'>
+          >
+            {roles.map(({ value, name }) => (
+              <MenuItem key={value} value={value}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button sx={{ mt: 2 }} variant='outlined' type='submit'>
           {dictionary.send}
         </Button>
       </form>
